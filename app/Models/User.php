2531ed92282c617286d8b4f35041ad9d085fa767
VincentZyrellDelaCruz <vincentzyrelldelacruz@gmail.com>
2026-03-11 16:39:00 +0800

    Add npm, replace user id with uuid, add role, create factories and seeders

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,17 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, HasUlids, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -44,5 +47,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function role(): BelongsTo
+    {
+        return $this->belongsTo(Role::class);
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,11 +16,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
-        User::factory()->create([
+        /* User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
-        ]);
+        ]); */
+
+        $roles = [
+                'Admin', 'Punong Barangay', 'Secretary', 'Treasurer', 'Kagawad',
+                'SK Chair', 'Tanod', 'BHW', 'BDRRM Coordinator', 'Encoder',
+                'Auditor', 'Guest',
+        ];
+
+        foreach($roles as $role) {
+            Role::factory()->create([
+                'role_name' => $role,
+            ]);
+        }
+
+        User::factory(5)->create();
     }
 }
