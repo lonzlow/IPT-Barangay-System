77af8e0ce4eaa212c068315502b462eb665834b2
kennethroyvillamayor57000@gmail.com <kennethroyvillamayor57000@gmail.com>
2026-05-20 15:36:06 +0800

    Logo Change

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,7 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Login | Barangay New Era</title>
+
+    <!-- Favicon / Webpage Icon -->
+    <link rel="icon" type="image/x-icon" href="{{ asset('images/logo/Barangay New Era Logo.jpg') }}">
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
